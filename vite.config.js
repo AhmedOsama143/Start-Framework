b357493dcf01e7682e7c8aa98d8b7b19769ca724
AhmedOsama143 <ao8911323@gmail.com>
2025-07-24 11:32:51 +0300

    edit-

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Start-Framework/", // ← لازم يكون اسم الريبو بالظبط هنا
+  base: "/Start-Framework/",
   plugins: [react()],
 });
